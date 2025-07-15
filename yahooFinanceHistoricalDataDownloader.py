03b72c87766bb65f2f5ebc176a37769d07618f0f
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2025-07-14 22:38:00 -0700

    remove unnecesary dict

--- yahooFinanceHistoricalDataDownloader.py
+++ yahooFinanceHistoricalDataDownloader.py
@@ -689,7 +689,6 @@ def executeCommand(stock:dict,dates:list[str],attributes:list[str],catagoryLooku
 def processStocks(commands:list[dict],stocks:list[dict]):
     print("executing commands...\n")
     #need to update the month reformat logic
-    months={1:"Jan",2:"Feb",3:"Mar",4:"Apr",5:"May",6:"Jun",7:"Jul",8:"Aug",9:"Sep",10:"Oct",11:"Nov",12:"Dec"}
     buffer=[]
     #if we have something to do
     if(len(commands)>0):
