6997db4f302926f9212d525615c4e1091b978cc4
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2025-08-03 15:51:27 -0700

    Update yahooFinanceHistoricalDataDownloader.py

--- yahooFinanceHistoricalDataDownloader.py
+++ yahooFinanceHistoricalDataDownloader.py
@@ -889,8 +889,9 @@ def updateCategories(newCategories:list, oldCategories:list, values:list[list])-
 
 def insertValue(date:date, value:str, category:int, categoryLookupDict:dict, values:list[list])->None:
     #find where to insert it and make sure that all hte category lists are equal size
-    point=findDateInsertionPoint(date,values[0])
     equalizeListLens(values)
+    point=findDateInsertionPoint(date,values[0])
+    
     #i am telling you linter, this will be an int, i wrote the code that makes the dict
     categoryIndex:int=categoryLookupDict[category]
     #if we are not creating a new spot for it
