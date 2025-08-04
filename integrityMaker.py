411ef7017019f0f552a52cf25f0b5d190efae03a
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2025-08-04 02:25:35 -0700

    upgrade obfiscation code

--- integrityMaker.py
+++ integrityMaker.py
@@ -3,6 +3,7 @@ import base64
 import random
 import string
 import os
+import random
 def makeSalt():
     saltMine=list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~")
 
@@ -47,17 +48,35 @@ def makeSillyString():
             hashes[writeIndex]=digisalt.decode()
             writeIndex+=1
 
-    
+    decoders=[]
+    nuhashes=[]
+    for hash in hashes:
+        shuffled=shuffle(list(hash))
+        decoders.append(shuffled[1])
+        nuhashes.append("".join(shuffled[0]))
 
     
-    print("[",end="")
+    print("(",end="")
     for index,name in enumerate(hashes):
         print("\""+name+"\"",end="")
         if(index<(len(hashes)-1)):
             print(", ",end="")
-    print("]")
-    
+        else:
+            print(")")
+
+    message5=["("]
+    for index, char in enumerate(decoders):
+        
+        message5.append(str(char))
+   
+        if(index<(len(decoders)-1)):
+            message5.append(", ")
+            
+            message5.append("\n")
+        else:
+            message5.append(")")
     print("\n"*2)
+    print("".join(message5))
 
 def makeFileString():
     files=os.listdir("renderer/firefox")
@@ -83,5 +102,58 @@ def makeFileString():
     
     print("".join(displayList))
         
-makeFileString()
+def shuffle(inputList:list[str])->tuple[list[str],list[int]]:
+    #make a copy of our input
+    copyList=inputList.copy()
+    #create a list of indexes
+    randomPosList=list(range(len(copyList)))
+    #shuffle it
+    random.shuffle(randomPosList)
+    #put the items in copyList in the shuffled order
+    scrambledList = [copyList[i] for i in randomPosList]
+    return (scrambledList,randomPosList)
+
+#makeFileString()
 #makeSillyString()
+def makeSecurityErrorMessage():
+    import dependencies.easyCLI as easyCLI
+    errorMessage=easyCLI.multilineStringBuilder(("ERROR: integrity check failed, License file(s) not found.",
+                "This program is open source and must be distributed with its licenses.",
+                "Please ensure the LICENSE.txt is present in the program's root folder, and", 
+                "the LICENSES directory is present and contains: \"easyCLI-GPL3-LICENSE.txt\",", 
+                "\"libxml2-MIT-LICENSE.txt\", \"libxslt-MIT-LICENSE.txt\", \"Python-PSFL-2-LICENSE.txt\",", 
+                "\"Mozilla-Public-License-2.0-LICENSE.txt\", \"Playwright-Apache-2.0-LICENSE.txt\",", 
+                "\"Nuitka-Apache-2.0-LICENSE.txt\", and \"lxml-BSD-3-Clause-LICENSE.txt\"."))
+    message2=list(base64.b85encode(errorMessage.encode()).decode())
+    
+    shuffled=shuffle(message2)
+    message2=shuffled[0]
+    message4=shuffled[1]
+    message4.reverse()
+    message3=["("]
+    for index, char in enumerate(message2):
+        message3.append("\"")
+        message3.append(char)
+        message3.append("\"")
+        if(index<(len(message2)-1)):
+            message3.append(", ")
+            if(index%20==0):
+                message3.append("\n")
+        else:
+            message3.append(")")
+    
+    message5=["("]
+    for index, char in enumerate(message4):
+        message5.append("\"")
+        message5.append(str(char))
+        message5.append("\"")
+        if(index<(len(message2)-1)):
+            message5.append(", ")
+            if(index%20==0):
+                message5.append("\n")
+        else:
+            message5.append(")")
+    print("".join(message3))
+    print("\n\n\n")
+    print("".join(message5))
+makeSecurityErrorMessage()
