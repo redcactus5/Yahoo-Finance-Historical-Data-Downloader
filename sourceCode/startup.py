33a59dfb7df1fb6c9950f7830264bbb6a14b7291
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2025-08-04 00:25:07 -0700

    change v number

--- sourceCode/startup.py
+++ sourceCode/startup.py
@@ -170,7 +170,7 @@ class YahooFinanceGrabberHeader(easyCLI.UIHeaderClass):
     #simple header class required by easy cli
     def __init__(self):
         super().__init__(None)
-        self.vNumber="v1.4"
+        self.vNumber="v1.3.1"
         self.vString="Yahoo Finance Historical Data Downloader "+self.vNumber+" by redcacus5"+"\n\n"
 
     def drawUIHeader(self):
