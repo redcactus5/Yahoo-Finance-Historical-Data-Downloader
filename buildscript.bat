c5d0a332345284c8d1d0dd9abe2163ff1c3006f1
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2025-08-05 18:33:47 -0700

    Update buildscript.bat

--- buildscript.bat
+++ buildscript.bat
@@ -1,2 +1,2 @@
 
-python -m nuitka --onefile --windows-icon-from-ico="yahoo finance downloader logov2.ico" yahooFinanceHistoricalDataDownloader.py
+python -m nuitka --mingw64 --onefile --windows-icon-from-ico="yahoo finance downloader logov2.ico" yahooFinanceHistoricalDataDownloader.py
